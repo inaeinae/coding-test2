32af46c5e0c318bfdb131b98c0bf62638b7c18f7
inaeinae <kkin0905@gmail.com>
2025-12-12 00:21:10 +0900

    [BJ] 큰 수 (BIG) / lv5

---
+++ BaekJoon/BJ_14928.java
@@ -0,0 +1,25 @@
+package inaeinae.studycodingtest.BaekJoon;
+
+import java.io.*;
+
+/**
+ * [BJ] 큰 수 (BIG) / lv5
+ * (2025.12.11) https://www.acmicpc.net/problem/14928
+ */
+public class BJ_14928 {
+    public void solution() throws IOException {
+        // N % M == (N * 10 + digit) % M
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+
+        int result = 0;
+        for (char c : input.toCharArray()) {
+            result = (result * 10 + (c - '0')) % 20000303;
+        }
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+    }
+}
--- StudyCodingTestApplication.java
+++ StudyCodingTestApplication.java
@@ -1,5 +1,6 @@
 package inaeinae.studycodingtest;
 
+import inaeinae.studycodingtest.BaekJoon.BJ_14928;
 import inaeinae.studycodingtest.Doit.No20_P2751;
 import inaeinae.studycodingtest.PGS.*;
 import org.springframework.boot.SpringApplication;
@@ -11,9 +12,7 @@ import java.io.IOException;
 public class StudyCodingTestApplication {
 
 	public static void main(String[] args) throws IOException {
-		PGS_42627 pgs42627 = new PGS_42627();
-		System.out.println("expect : 9, result : " + pgs42627.solution(new int[][]{{1, 9}, {0, 3}, {2, 6}}));
-		System.out.println("expect : 72, result : " + pgs42627.solution(new int[][]{{24, 10}, {28, 39}, {43, 20}, {37, 5}, {47, 22}, {20, 47}, {15, 34}, {15, 2}, {35, 43}, {26, 1}}));
-		System.out.println("expect : 6, result : " + pgs42627.solution(new int[][]{{0, 5}, {2, 10}, {10000, 2}}));
+		BJ_14928 bj14928 = new BJ_14928();
+		bj14928.solution();
 	}
 }
