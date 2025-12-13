133a3c8d3c05d51fb231d4f3886d6df4bcd5ab82
inaeinae <kkin0905@gmail.com>
2025-12-13 15:42:56 +0900

    [BJ] 삼각형 / lv5

--- StudyCodingTestApplication.java
+++ StudyCodingTestApplication.java
@@ -2,6 +2,7 @@ package inaeinae.studycodingtest;
 
 import inaeinae.studycodingtest.BaekJoon.BJ_14928;
 import inaeinae.studycodingtest.BaekJoon.BJ_27889;
+import inaeinae.studycodingtest.BaekJoon.BJ_28113;
 import inaeinae.studycodingtest.BaekJoon.BJ_29751;
 import inaeinae.studycodingtest.Doit.No20_P2751;
 import inaeinae.studycodingtest.PGS.*;
@@ -14,7 +15,7 @@ import java.io.IOException;
 public class StudyCodingTestApplication {
 
 	public static void main(String[] args) throws IOException {
-		BJ_29751 bj29751 = new BJ_29751();
-		bj29751.solution();
+		BJ_28113 bj28113 = new BJ_28113();
+		bj28113.solution();
 	}
 }
